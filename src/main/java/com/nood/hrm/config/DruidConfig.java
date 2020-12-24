352ccbb305785fa7b3b047a0e5d72b72fe59e49d
Yanmeng <ymeng125@126.com>
2020-12-24 23:07:39 +0800

    with dynamic datasource

--- src/main/java/com/nood/hrm/config/DruidConfig.java
+++ src/main/java/com/nood/hrm/config/DruidConfig.java
@@ -5,12 +5,12 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import javax.sql.DataSource;
-
-@Configuration
-public class DruidConfig {
-    @Bean(destroyMethod = "close", initMethod = "init")
-    @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSource getDataSource(){
-        return new DruidDataSource();
-    }
-}
+//
+//@Configuration
+//public class DruidConfig {
+//    @Bean(destroyMethod = "close", initMethod = "init")
+//    @ConfigurationProperties(prefix = "spring.datasource")
+//    public DataSource getDataSource(){
+//        return new DruidDataSource();
+//    }
+//}
